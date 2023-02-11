5c5bb4fdcfa9ba9fcb04f67d2fea38e5d8c1fcca
Gihan Wijesinghe <gihanwijesinghe12@gmail.com>
2023-02-11 23:33:04 +0530

    added db models

---
+++ GPStarAPI/Models/Invoice.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GPStarAPI.Models
+{
+    public class Invoice
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public DateTime Date { get; set; }
+        public decimal TotalAmount { get; set; }
+        public ICollection<InvoiceLine> InvoiceLines { get; set; }
+    }
+}
---
+++ GPStarAPI/Models/InvoiceLine.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GPStarAPI.Models
+{
+    public class InvoiceLine
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal LinePrice { get; set; }
+        public Invoice Invoice { get; set; }
+    }
+}
