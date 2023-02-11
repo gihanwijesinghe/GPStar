766fdbc6d6ff19e38aefc509ad7a1e38aefaaa95
Gihan Wijesinghe <gihanwijesinghe12@gmail.com>
2023-02-11 23:34:17 +0530

    scafolding

---
+++ GPStarAPI/Controllers/InvoicesController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GPStarAPI.Data;
+using GPStarAPI.Models;
+using GPStarAPI.Systems;
+using GPStarAPI.ApiModels;
+
+namespace GPStarAPI
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvoicesController : ControllerBase
+    {
+        private readonly GPStarContext _context;
+        private readonly InvoiceSystem _invoiceSystem;
+
+        public InvoicesController(GPStarContext context, InvoiceSystem invoiceSystem)
+        {
+            _context = context;
+            _invoiceSystem = invoiceSystem;
+        }
+
+        // GET: api/Invoices
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Invoice>>> GetInvoice()
+        {
+            return await _context.Invoices.ToListAsync();
+        }
+
+        // GET: api/Invoices/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<InvoiceGet>> GetInvoice(Guid id)
+        {
+            var invoice = await _invoiceSystem.GetInvoiceById(id);
+
+            if (invoice == null)
+            {
+                return NotFound();
+            }
+
+            return invoice;
+        }
+
+        // PUT: api/Invoices/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutInvoice(Guid id, Invoice invoice)
+        {
+            if (id != invoice.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(invoice).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!InvoiceExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Invoices
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Guid>> PostInvoice(InvoicePost invoicePost)
+        {
+            var id = await _invoiceSystem.CreateInvoice(invoicePost);
+
+            return CreatedAtAction("GetInvoice", new { id = id }, id);
+        }
+
+        private bool InvoiceExists(Guid id)
+        {
+            return _context.Invoices.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ GPStarAPI/Data/GPStarContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using GPStarAPI.Models;
+
+namespace GPStarAPI.Data
+{
+    public class GPStarContext : DbContext
+    {
+        public GPStarContext(DbContextOptions<GPStarContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<GPStarAPI.Models.Invoice> Invoices { get; set; }
+        public DbSet<GPStarAPI.Models.InvoiceLine> InvoiceLines { get; set; }
+    }
+}
